5093464b27af4deb19198faa3b7d4c024dbcba7b
Dragnell <bang2001vl@outlook.com.vn>
2021-12-18 17:07:14 +0700

    Update result_screen.dart

--- sathachlaixe/lib/UI/Test/result_screen.dart
+++ sathachlaixe/lib/UI/Test/result_screen.dart
@@ -31,85 +31,103 @@ class ResultTest extends StatelessWidget {
 
     String title = "Chúc mừng!";
     String msg = "Bạn đã đạt bài thi này,\n Chúc bạn có kết quả như mong đợi!";
-    String imageURL = "assets/icons/key.png";
+    String backgroundURL = "assets/images/result_pass_bg.png";
 
     if (!history.isPassed) {
       title = "Thật tiếc!";
       msg = "Bạn đã trượt bài thi này, cố gắng lên nhé!";
+      backgroundURL = "assets/images/result_fail_bg.png";
     }
 
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: <Widget>[
-        SizedBox(
-          height: 100.h,
-        ),
-        Stack(alignment: Alignment.center, children: [
-          CustomPaint(painter: CustomCircularProgress(value: completeValue)),
-          Column(
-            children: [
-              Text(
-                completePercent,
-                style:
-                    kText35Bold_1.copyWith(fontSize: ScreenUtil().setSp(50.sp)),
+    String imageURL = "assets/icons/key.png";
+
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          height: size.height,
+          width: size.width,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            image: DecorationImage(
+                image: AssetImage(backgroundURL), fit: BoxFit.fill),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                height: 100.h,
+              ),
+              Stack(alignment: Alignment.center, children: [
+                CustomPaint(
+                    painter: CustomCircularProgress(value: completeValue)),
+                Column(
+                  children: [
+                    Text(
+                      completePercent,
+                      style: kText35Bold_1.copyWith(
+                          fontSize: ScreenUtil().setSp(50.sp)),
+                    ),
+                    Text(
+                      complete,
+                      style: kText20Medium_10,
+                    ),
+                  ],
+                )
+              ]),
+              SizedBox(
+                height: 30.h,
+              ),
+              Image.asset(imageURL),
+              SizedBox(
+                height: 20.h,
               ),
               Text(
-                complete,
+                title,
+                style: kText35Bold_14,
+              ),
+              SizedBox(
+                height: 10.h,
+              ),
+              Text(
+                msg,
+                textAlign: TextAlign.center,
                 style: kText20Medium_10,
               ),
+              SizedBox(
+                height: 60.h,
+              ),
+              GestureDetector(
+                child: Container(
+                  height: 60.h,
+                  width: 250.w,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(38),
+                    color: dtcolor1,
+                  ),
+                  alignment: Alignment.center,
+                  child: Text(
+                    'Xem chi tiết',
+                    style: kText22Bold_13,
+                  ),
+                ),
+                onTap: () => Navigator.pop(context, RESULT_REVIEW),
+              ),
+              SizedBox(
+                height: 15.h,
+              ),
+              GestureDetector(
+                child: Text(
+                  "Trở về trang chủ",
+                  style: kText18Bold_3,
+                ),
+                onTap: () => Navigator.pop(context, RESULT_CANCEL),
+              ),
             ],
-          )
-        ]),
-        SizedBox(
-          height: 30.h,
-        ),
-        Image.asset(imageURL),
-        SizedBox(
-          height: 20.h,
-        ),
-        Text(
-          title,
-          style: kText35Bold_14,
-        ),
-        SizedBox(
-          height: 10.h,
-        ),
-        Text(
-          msg,
-          textAlign: TextAlign.center,
-          style: kText20Medium_10,
-        ),
-        SizedBox(
-          height: 60.h,
-        ),
-        GestureDetector(
-          child: Container(
-            height: 60.h,
-            width: 250.w,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(38),
-              color: dtcolor1,
-            ),
-            alignment: Alignment.center,
-            child: Text(
-              'Xem chi tiết',
-              style: kText22Bold_13,
-            ),
           ),
-          onTap: () => Navigator.pop(context, RESULT_REVIEW),
         ),
-        SizedBox(
-          height: 15.h,
-        ),
-        GestureDetector(
-          child: Text(
-            "Trở về trang chủ",
-            style: kText18Bold_3,
-          ),
-          onTap: () => Navigator.pop(context, RESULT_CANCEL),
-        ),
-      ],
+      ),
     );
   }
 
@@ -141,22 +159,7 @@ class ResultTest extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var size = MediaQuery.of(context).size;
-    return Scaffold(
-      body: SafeArea(
-        child: Container(
-          height: size.height,
-          width: size.width,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            image: DecorationImage(
-                image: AssetImage("assets/images/result_pass_bg.png"),
-                fit: BoxFit.fill),
-          ),
-          child: buildContent(context, quizState),
-        ),
-      ),
-    );
+    return buildContent(context, quizState);
   }
 }
 
